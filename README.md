610f3387735ab95afbc7e65cab51400e579aa116
Patrick <corus87@users.noreply.github.com>
2018-12-11 13:26:10 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,66 @@
+# Porcupine trigger
+Trigger for Kalliope
+
+## Parameters
+
+| parameter    | required | type    | default | choices         | comment                                                                                          |
+|--------------|----------|---------|---------|-----------------|--------------------------------------------------------------------------------------------------|
+| keywords     | TRUE     | string  |         |                 |                                                                                                  |
+| ppn_file     | TRUE     | string  |         |                 | Path to the porcupine wake word. The path can be absolute or relative to the brain file          |
+| sensitivity  | FALSE    | string  | 0.5     | between 0 and 1 | Increasing the sensitivity value lead to better detection rate, but also higher false alarm rate |
+| input_device | FALSE    | integer | default | 				| Select the input input input device 															   |
+| tiny_keyword | FALSE 	  | string	| false   | true/false      | If true you can use tiny keywords, these accuracy is slightly lower than the standard model but it consumes considerably less resources |
+
+## Example settings
+
+```yaml
+# This is the trigger engine that will catch your magic work to wake up Kalliope. With porcupine we need different keywords for different platforms. The example use the wake word "porcupine" for the raspberry.
+
+default_trigger: "porcupine"
+
+# Trigger engine configuration
+triggers:
+  - porcupine:
+      keywords:
+        - keyword: 
+            ppn_file: "trigger/porcupine/porcupine_raspberrypi.ppn"
+
+# To use multiple keywords with different sensitivities
+triggers:
+  - porcupine:
+      keywords:  
+        - keyword: 
+            ppn_file: "trigger/porcupine/porcupine_raspberrypi.ppn"
+            sensitivity: 0.4
+        - keyword:
+            ppn_file: "trigger/porcupine/blueberry_raspberrypi.ppn"
+            sensitivity: 0.3
+```
+
+## Available Porcupine keyword
+
+You can find existing keywords [here](https://github.com/Picovoice/Porcupine/tree/master/resources/keyword_files). 
+To create your own wake word you have to use the [optimizer from the porcupine repository](https://github.com/Picovoice/Porcupine/tree/master/tools/optimizer). You can only create keywords for Linux, Mac and Windows and in our case we cannot create keywords for the raspberry. 
+If you use Kalliope on a x86_64 machine with Linux and want to create your own wake word you do it with the above commands. The wake word Kalliope will be saved in your home directory.
+Note, keywords created by the optimizer tool will expire after 90 days.
+
+```yaml
+git clone https://github.com/Picovoice/Porcupine.git
+cd Porcupine
+tools/optimizer/linux/x86_64/pv_porcupine_optimizer -r resources -w "Kalliope" -p linux -o ~/
+```
+
+## Note
+
+You have to add the path to the trigger folder in your settings.
+E.g.:
+```
+# ---------------------------
+# resource directory path
+# ---------------------------
+resource_directory:
+  trigger: "resources/trigger"
+  neuron: "resources/neurons"
+  stt: "resources/stt"
+  tts: "resources/tts"
+```
---
+++ porcupine.py
@@ -0,0 +1,124 @@
+import logging
+import os
+import sys
+from threading import Thread
+from kalliope import Utils
+from cffi import FFI as _FFI
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from porcupinedecoder import HotwordDetector
+
+class PorcupineWakeWordNotFound(Exception):
+    pass
+
+
+class MissingParameterException(Exception):
+    pass
+
+logging.basicConfig()
+logger = logging.getLogger("kalliope")
+
+
+class Porcupine(Thread):
+
+    def __init__(self, **kwargs):
+        super(Porcupine, self).__init__()
+        self._ignore_stderr()
+        # Pause listening boolean.
+        self.kill_received = False
+        # Get input device if set by the user.
+        self.input_device_index = kwargs.get('input_device_index', None)
+        # Use tiny keywords if user set to True.
+        self.tiny_keyword = kwargs.get('tiny_keyword', False)
+        # Callback function to call when hotword caught.
+        self.callback = kwargs.get('callback', None)
+        if self.callback is None:
+            raise MissingParameterException("Callback function is required with porcupine")
+        # Get keywords to load.
+        self.keywords = kwargs.get('keywords', None)
+
+        if self.keywords is None:
+            raise MissingParameterException("At least one keyword is required with porcupine")
+        
+        keyword_file_paths = list()
+        sensitivities = list()
+        
+        for keyword in self.keywords:
+            path = Utils.get_real_file_path(keyword['keyword']['ppn_file'])  
+            try:
+                os.path.isfile(path)
+            except TypeError: 
+                raise PorcupineWakeWordNotFound("The porcupine keyword at %s does not exist" % keyword['keyword']['ppn_file'])
+            try:
+                sensitivity = keyword['keyword']['sensitivity']
+            except KeyError:
+                sensitivity = 0.5
+            
+            keyword_file_paths.append(path)
+            sensitivities.append(sensitivity)
+            
+        keyword_file_paths = ", ".join(keyword_file_paths)
+        sensitivities = ", ".join(map(str, sensitivities))
+
+        self.detector = HotwordDetector(keyword_file_paths=keyword_file_paths,
+                                                        sensitivities=sensitivities,
+                                                        input_device_index=self.input_device_index,
+                                                        tiny=self.tiny_keyword,
+                                                        detected_callback=self.callback
+                                                        )
+
+    def run(self):
+        """
+        Start the porcupine thread and wait for a Kalliope trigger word
+        :return:
+        """
+        # start porcupine loop forever
+        self.detector.daemon = True
+        self.detector.start()
+        self.detector.join()
+
+    def pause(self):
+        """
+        pause the porcupine main thread
+        """
+        logger.debug("[Porcupine] Pausing porcupine process")
+        self.detector.paused = True
+
+    def unpause(self):
+        """
+        unpause the porcupine main thread
+        """
+        logger.debug("[Porcupine] Unpausing porcupine process")
+        self.detector.paused = False
+
+    def stop(self):
+        """
+        Kill the porcupine process
+        :return: 
+        """
+        logger.debug("[Porcupine] Killing porcupine process")
+        self.interrupted = True
+        self.detector.terminate()
+
+    @staticmethod
+    def _ignore_stderr():
+        """
+        Try to forward PortAudio messages from stderr to /dev/null.
+        """
+        ffi = _FFI()
+        ffi.cdef("""
+            /* from stdio.h */
+            FILE* fopen(const char* path, const char* mode);
+            int fclose(FILE* fp);
+            FILE* stderr;  /* GNU C library */
+            FILE* __stderrp;  /* Mac OS X */
+            """)
+        stdio = ffi.dlopen(None)
+        devnull = stdio.fopen(os.devnull.encode(), b'w')
+        try:
+            stdio.stderr = devnull
+        except KeyError:
+            try:
+                stdio.__stderrp = devnull
+            except KeyError:
+                stdio.fclose(devnull)
